fe906846bde3912915341cb5ef6a30b6e1932a25
TitoFF <9.titos22@gmail.com>
2019-06-12 00:11:36 -0400

    mi respuesta a funciones codificacion y decodificacion

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,10 @@
 window.cipher = {
-  encode: () => {
+  encode: (texto_a_codificar, offset) => {
     /* Acá va tu código */
-    
+    texto_a_codificar.split("").map(function(x){return (String.fromCharCode((x.charCodeAt(0)-65+offset)%26+65)).join().replace(/,/g, "") })
   },
-  decode: () => {
+  decode: (texto_a_codificar, offset) => {
     /* Acá va tu código */
+	texto_a_codificar.split("").map(function(x){return (String.fromCharCode((x.charCodeAt(0)-65-(offset%26)+26)%26+65)).join().replace(/,/g, "") });
   }
 };
